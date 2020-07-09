2deee2ad0a986f0df663c97075607c7054dd69a6
mryysbll19 <maryysabellepurohayahay@gmail.com>
2020-07-09 18:54:27 +0800

    added dynamic css

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 new Vue({
     el:'#vue-app',
     data:{
-       age:20,
-       a: 0,
-       b: 0
+     available: false,
+     nearby: false
     },
- computed:{
-        addToA: function(){
-            console.log ('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log ('addToB');
-            return this.b + this.age;
-        },
-      }
-});
+    methods: {
+
+    },
+    computed: {
+        compClasses:function(){
+            return{
+                available: this.available,
+                nearby: this.nearby
+            },
+        }
