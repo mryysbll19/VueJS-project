238eefdf25ffd737df1a1f11bdfd7db9fde642dd
mryysbll19 <maryysabellepurohayahay@gmail.com>
2020-07-10 18:51:39 +0800

    simple punching bag game

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 new Vue({
     el:'#vue-app',
     data:{
-        characters: ['Joneil', 'Ysabelle', 'Asawa'],
-        ninjas: [
-            { name: 'joneil', age: 28},
-            { name: 'ysa', age: 23},
-            { name: 'asawa', age: 2}
-        ]
+        health: 100,
+        ended: false
     },
-    methods: {
-
-    },
+    methods:{
+        punch: function(){
+            this.health-= 10;
+            if (this.health <= 0 ){
+                this.ended= true;
+            }
+        },
+    restart: function(){
+        this.health= 100;
+        this.ended= false;
+    }
+  },
     computed: {
+
     
             }
 
